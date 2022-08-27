1e92a767cb2e51cef33f920b0044bafc3498eaeb
Pablo Carballo <pmcarballo@gmail.com>
2022-08-27 11:11:39 -0300

    Added XML comments to the extension classes.

--- src/RomanNumbers/Extensions/Int32Extensions.cs
+++ src/RomanNumbers/Extensions/Int32Extensions.cs
@@ -2,6 +2,11 @@ namespace RomanNumbers.Extensions;
 
 public static class Int32Extensions
 {
+    /// <summary>
+    /// Counts the number of digits.
+    /// </summary>
+    /// <param name="number">The number whose digits will be counted.</param>
+    /// <returns>The count of digits.</returns>
     public static int Length(this int number)
     {
         int count = 0;
--- src/RomanNumbers/Extensions/StringExtensions.cs
+++ src/RomanNumbers/Extensions/StringExtensions.cs
@@ -4,6 +4,12 @@ namespace RomanNumbers.Extensions;
 
 public static class StringExtensions
 {
+    /// <summary>
+    /// Counts the number of times a Roman symbol appears in a Roman Number.
+    /// </summary>
+    /// <param name="romanNumber">The Roman representation to analyze.</param>
+    /// <param name="romanDigit">The Roman symbol to be searched.</param>
+    /// <returns>The number of times.</returns>
     public static int DigitCount(this string romanNumber, string romanDigit)
     {
         return Regex.Matches(romanNumber, romanDigit).Count;
